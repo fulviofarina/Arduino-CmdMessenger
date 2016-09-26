e2eccc24feb88d4e010e5b091a8e347141c7803f
fulviofarina <fulviofarina@gmail.com>
2016-09-26 01:58:47 -0400

    test

---
+++ examples/SP/Debug/SP.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\Fulvio\AppData\Local\Temp\VMicroBuilds\SP\uno\SP.cpp.o: \
+ C:\Users\Fulvio\AppData\Local\Temp\VMicroBuilds\SP\uno\SP.cpp \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/arduino.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/binary.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/WCharacter.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/WString.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/HardwareSerial.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/Stream.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/Print.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/Printable.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/USBAPI.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/Arduino.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\variants\standard/pins_arduino.h \
+ D:\Dropbox\My\ Software\Arduino\libraries\CmdMessenger/CmdMessenger.h \
+ C:\Users\Fulvio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.12\cores\arduino/Arduino.h
